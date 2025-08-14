b018151a5a79785291e205c9e2be19a7ba08a66e
Neil Huyton <github@nehu.me>
2025-08-14 12:03:27 +0100

    chore: remove unused files

--- netlify/functions/hello.js
+++
@@ -1,7 +0,0 @@
-// netlify/functions/hello.js
-export const handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Netlify Functions!" }),
-  };
-};

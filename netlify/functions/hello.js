7007e11687e106fae6104479ab7ed97319facdf0
Neil Huyton <github@nehu.me>
2025-08-14 11:19:57 +0100

    feat: remove error

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,5 +1,5 @@
 // netlify/functions/hello.js
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello from Netlify Functions!" }),
